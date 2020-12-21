30a03740e4e8ac601459b01be8b09acc5629aab0
AquilesDias <kilesax@gmail.com>
2020-12-21 00:21:28 -0300

    Aplicando rotas e componente crud

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatCardModule} from '@angular/material/card';
 
 import {MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HomeComponent } from './views/home/home.component';
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCrudComponent
   ],
   imports: [
     BrowserModule,
