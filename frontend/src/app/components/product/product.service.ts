1aff41d931d734250603723fb7c03ef40c57d656
AquilesDias <kilesax@gmail.com>
2020-12-22 11:33:53 -0300

    Componente read

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 })
 export class ProductService {
 
-  baseUrl =  "http://localhost:3001/products"
+  baseUrl = "http://localhost:3001/products"
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
@@ -23,6 +23,10 @@ export class ProductService {
   }
 
   create(product: Product): Observable<Product>{
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product);
   }
+
+   read(): Observable<Product[]> {
+     return this.http.get<Product[]>(this.baseUrl);
+   }
 }
